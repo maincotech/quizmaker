657cef917b38d4f0731b5ecf18653e23c7334fee
maincotech <67356804+maincotech@users.noreply.github.com>
2021-04-23 19:43:50 +0900

    add authentication

--- src/Maincotech.ExamAssitant.Quizmaker/Startup.cs
+++ src/Maincotech.ExamAssitant.Quizmaker/Startup.cs
@@ -6,13 +6,17 @@ using Maincotech.Quizmaker.Services;
 using Maincotech.Storage;
 using Maincotech.Storage.AzureBlob;
 using Maincotech.Web.Components.Vditor;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -32,6 +36,25 @@ namespace Maincotech.Quizmaker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
+                options.HandleSameSiteCookieCompatibility();
+            });
+
+            // Configuration to sign-in users with Azure AD B2C
+            services.AddMicrosoftIdentityWebAppAuthentication(Configuration, "AzureAdB2C");
+
+            services.AddControllersWithViews()
+                .AddMicrosoftIdentityUI();
+            //Configuring appsettings section AzureAdB2C, into IOptions
+            services.AddOptions();
+            services.Configure<OpenIdConnectOptions>(Configuration.GetSection("AzureAdB2C"));
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddAntDesign();
@@ -93,13 +116,20 @@ namespace Maincotech.Quizmaker
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
+
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
+            {                
+                //endpoints.MapControllerRoute(
+                //  name: "Account",
+                //  pattern: "Account/{action}/{id?}");
                 endpoints.MapControllerRoute("api", "api/{controller}/{action}/{id?}");
+                endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
 
